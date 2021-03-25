b0bde6fef5239efee9c4ae0b452464c62abb67e1
Jair Novello <jaja505@hotmail.com>
2021-03-25 10:47:11 -0400

    @DisplayName annotation for Junit5_intro

--- src/test/java/day1/Junit5_intro.java
+++ src/test/java/day1/Junit5_intro.java
@@ -1,12 +1,29 @@
 package day1;
 
-import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+//setting display name of the test class in test result using @DisplayName
+@DisplayName("Day 1 of Junit5 Test")
 public class Junit5_intro {
 
+    @DisplayName("Testing Numbers")
     @Test
     public void test(){
-        System.out.println("hello");
+        System.out.println("Learning JUnit5");
+
+        assertEquals(1,2);
+    }
+
+    @DisplayName("Testing name start with J")
+    @Test
+    public void testName(){
+        String name = "Jair";
+        //assertEquals('A', name.charAt(0));
+        assertTrue(name.startsWith("J"));
+
     }
 
 
