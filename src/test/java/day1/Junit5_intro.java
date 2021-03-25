22822b6f4d8d896205da94676a51dcdc61cb2050
Jair Novello <jaja505@hotmail.com>
2021-03-25 10:24:27 -0400

    Initial commit

---
+++ src/test/java/day1/Junit5_intro.java
@@ -0,0 +1,18 @@
+package day1;
+
+import org.junit.jupiter.api.Test;
+
+public class Junit5_intro {
+
+    @Test
+    public void test(){
+        System.out.println("hello");
+    }
+
+
+
+
+
+
+
+}
